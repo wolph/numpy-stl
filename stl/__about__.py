84fbba56ac01398447c818130e5fc51df6c9145a
Rick van Hattem <Wolph@wol.ph>
2016-03-26 12:47:21 +0100

    added support for empty lines in stl files thanks to @ncloward

--- stl/__about__.py
+++ stl/__about__.py
@@ -1,6 +1,6 @@
 __package_name__ = 'numpy-stl'
 __import_name__ = 'stl'
-__version__ = '1.7.1'
+__version__ = '1.8.0'
 __author__ = 'Rick van Hattem'
 __author_email__ = 'Wolph@Wol.ph'
 __description__ = '''
