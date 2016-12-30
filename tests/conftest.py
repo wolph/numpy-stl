9d92a8544c8be8eef5150bd4d0d8b70ed0886375
Miro Hrončok <miro@hroncok.cz>
2016-12-30 17:09:00 +0100

    Tests: Use session scoped fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,26 +8,26 @@ def pytest_generate_tests(metafunc):
     metafunc.parametrize('speedups', [False, True])
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def cwd():
     return py.path.local(__file__).dirpath()
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def ascii_path(cwd):
     return cwd.join('stl_ascii')
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def binary_path(cwd):
     return cwd.join('stl_binary')
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def ascii_file(ascii_path):
     return str(ascii_path.join('HalfDonut.stl'))
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def binary_file(binary_path):
     return str(binary_path.join('HalfDonut.stl'))
