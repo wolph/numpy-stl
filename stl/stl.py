eb9f0538a2e9f46d9ee2fa2226b8fc8395ff5fcf
Rick van Hattem <Wolph@wol.ph>
2017-05-03 22:43:55 +0200

    working around issue #52

--- stl/stl.py
+++ stl/stl.py
@@ -211,7 +211,12 @@ class BaseStl(base.BaseMesh):
     @classmethod
     def _load_ascii(cls, fh, header, speedups=True):
         if _speedups and speedups:
-            return _speedups.ascii_read(fh, header)
+            try:
+                return _speedups.ascii_read(fh, header)
+            except RuntimeError:
+                # We got an unexpected error, retrying without speedups
+                # Related to bug #52
+                return cls._load_ascii(fh, header, speedups=False)
         else:
             iterator = cls._ascii_reader(fh, header)
             name = next(iterator)
