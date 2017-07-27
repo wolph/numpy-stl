ad6cbbc0a49c427004b8749a10d5af5697e73f7b
Rick van Hattem <Wolph@wol.ph>
2017-07-27 19:39:33 +0200

    cython does not fully support python 3 on windows yet. This fixes #54

--- setup.py
+++ setup.py
@@ -8,22 +8,23 @@ from setuptools.command.build_ext import build_ext
 
 setup_kwargs = {}
 
-try:
-    import numpy
-    from Cython import Build
+if sys.version_info.major != 3 or sys.platform.lower() != 'win32':
+    try:
+        import numpy
+        from Cython import Build
 
-    setup_kwargs['ext_modules'] = Build.cythonize([
-        extension.Extension(
-            'stl._speedups',
-            ['stl/_speedups.pyx'],
-            include_dirs=[numpy.get_include()],
-        ),
-    ])
-except ImportError:
-    print('WARNING', file=sys.stderr)
-    print('Cython and Numpy is required for building extension.',
-          file=sys.stderr)
-    print('Falling back to pure Python implementation.', file=sys.stderr)
+        setup_kwargs['ext_modules'] = Build.cythonize([
+            extension.Extension(
+                'stl._speedups',
+                ['stl/_speedups.pyx'],
+                include_dirs=[numpy.get_include()],
+            ),
+        ])
+    except ImportError:
+        print('WARNING', file=sys.stderr)
+        print('Cython and Numpy is required for building extension.',
+            file=sys.stderr)
+        print('Falling back to pure Python implementation.', file=sys.stderr)
 
 # To prevent importing about and thereby breaking the coverage info we use this
 # exec hack
