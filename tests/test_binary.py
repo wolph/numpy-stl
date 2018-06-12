ba621131803ad94bcfdf24f8b26c3f92932f5c6d
Rick van Hattem <Wolph@wol.ph>
2018-06-12 19:21:09 +0200

    added tests for forced ascii mode

--- tests/test_binary.py
+++ tests/test_binary.py
@@ -1,9 +1,26 @@
-from stl import mesh
+import pytest
+from stl import mesh, Mode
 
 
-def test_ascii_like_binary(tmpdir, speedups):
-    mesh.Mesh.from_file('tests/stl_binary/rear_case.stl', speedups=speedups)
+@pytest.mark.parametrize('mode', [Mode.BINARY, Mode.AUTOMATIC])
+def test_ascii_like_binary(tmpdir, speedups, mode):
+    _test(tmpdir, speedups, mode, False)
+    _test(tmpdir, speedups, mode, True)
 
-    with open('tests/stl_binary/rear_case.stl', 'rb') as fh:
-        mesh.Mesh.from_file('rear_case.stl', fh=fh, speedups=speedups)
 
+def test_binary_in_ascii_mode(tmpdir, speedups):
+    with pytest.raises(RuntimeError):
+        _test(tmpdir, speedups, mode=Mode.ASCII, use_filehandle=False)
+
+    with pytest.raises(RuntimeError):
+        _test(tmpdir, speedups, mode=Mode.ASCII, use_filehandle=True)
+
+
+def _test(tmpdir, speedups, mode, use_filehandle=True):
+    if use_filehandle:
+        with open('tests/stl_binary/rear_case.stl', 'rb') as fh:
+            mesh.Mesh.from_file('rear_case.stl', fh=fh, speedups=speedups,
+                                mode=mode)
+    else:
+        mesh.Mesh.from_file('tests/stl_binary/rear_case.stl',
+                            speedups=speedups, mode=mode)
