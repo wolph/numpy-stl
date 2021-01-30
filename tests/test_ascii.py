22f89d6ddfe739fdeafabb1d5ffdac4bd6d22520
Rick van Hattem <Wolph@wol.ph>
2021-01-30 18:03:01 +0100

    added test for non-ascii and mixed case names

--- tests/test_ascii.py
+++ tests/test_ascii.py
@@ -8,8 +8,31 @@ from stl.utils import b
 from stl import mesh
 
 
+def test_chinese_name(tmpdir, speedups):
+    name = 'Test Chinese name 月球'
+    _stl_file = ('''
+    solid %s
+      facet normal -0.014565 0.073223 -0.002897
+        outer loop
+          vertex 0.399344 0.461940 1.044090
+          vertex 0.500000 0.500000 1.500000
+          vertex 0.576120 0.500000 1.117320
+        endloop
+      endfacet
+    endsolid
+    ''' % name).lstrip()
+
+    tmp_file = tmpdir.join('tmp.stl')
+    with tmp_file.open('wb+') as fh:
+        fh.write(b(_stl_file))
+        fh.seek(0)
+        test_mesh = mesh.Mesh.from_file(str(tmp_file), fh=fh,
+                                        speedups=speedups)
+        assert test_mesh.name == b(name)
+
+
 def test_long_name(tmpdir, speedups):
-    name = 'just some very long name which will not fit within the standard'
+    name = 'Just Some Very Long Name which will not fit within the standard'
     name += name
     _stl_file = ('''
     solid %s
