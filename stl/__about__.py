7e269ece8de2519a75c6173792c4d36380842996
Rick van Hattem <Wolph@wol.ph>
2021-02-09 02:45:19 +0100

    Incrementing version to v2.15.1

--- stl/__about__.py
+++ stl/__about__.py
@@ -1,6 +1,6 @@
 __package_name__ = 'numpy-stl'
 __import_name__ = 'stl'
-__version__ = '2.15.0'
+__version__ = '2.15.1'
 __author__ = 'Rick van Hattem'
 __author_email__ = 'Wolph@Wol.ph'
 __description__ = ' '.join('''
